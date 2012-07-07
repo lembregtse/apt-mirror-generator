b6fed3b91e56b7b945b87e1cc03762876c4ac9b9
Eric Lembregts <eric@lembregts.eu>
2012-07-07 10:04:34 +0300

    Add usage function and disclaimer

--- mirror.generate.sh
+++ mirror.generate.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+# This script generates an apt-mirror valid config file 
+# based on a local config file as specified in mirror.config.example
+#
+# Written by Eric Lembregts <eric@lembregts.eu>
+# Project @ http://www.github.com/lembregtse/apt-mirror-generator
+
+function usage() {
+cat - >&2 <<EOF
+mirror.generate.sh, create an apt-mirror config file.
+   usage: mirror.generate.sh [mirror.generate.sh config file]
+EOF
+  exit 1
+}
+
+if test "$?" -ne 0; then
+  usage
+fi
+
 FILENAME=$1 #argument 1 = filename of config
-
-
-
